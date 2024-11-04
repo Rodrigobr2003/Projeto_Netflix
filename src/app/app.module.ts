168a664f47e7ce0d06742b11f6f3d1d3c77fe87b
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-04 11:23:38 -0300

    :bricks: feat: Adiciona estrutura nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { OptionsComponent } from './nav-bar/options/options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule,
