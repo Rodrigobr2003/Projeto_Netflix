d86f06317b2504dbb22a88316a557ffd6f178987
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-04 17:04:47 -0300

    :lipstick: feat: Ajusta layout navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
 import { OptionsComponent } from './nav-bar/options/options.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    OptionsComponent
+    OptionsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/nav-bar/options/options.component.ts
+++ src/app/nav-bar/options/options.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
-  styleUrl: './options.component.css'
+  styleUrl: './options.component.css',
 })
 export class OptionsComponent {
-
+  faLanguage = faLanguage;
 }
