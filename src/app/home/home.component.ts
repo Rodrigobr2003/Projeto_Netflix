4563d8ceff15a812132d627b41dc5b6d0493820c
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-07 17:06:02 -0300

    :lipstick: feat: Adiciona topo da home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { faPlay, faInfo } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  play = faPlay;
+  info = faInfo;
 }
