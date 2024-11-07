32441edb229e7e0892b50e38ebe563193e35e74f
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-07 15:14:20 -0300

    :wrench: feat: Adiciona lógica para exibição de componentes

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css',
 })
-export class NavBarComponent {}
+export class NavBarComponent implements OnInit {
+  actvRoute: Router;
+  showOptions: boolean = false;
+  showProfileOptions: boolean = false;
+
+  constructor() {
+    this.actvRoute = inject(Router);
+  }
+
+  ngOnInit(): void {
+    if (this.actvRoute.url.includes('/home')) {
+      this.showOptions = false;
+      this.showProfileOptions = true;
+    } else {
+      this.showOptions = true;
+      this.showProfileOptions = false;
+    }
+  }
+}
