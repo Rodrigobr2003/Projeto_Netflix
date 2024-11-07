2be7e01d94357099499b973173c566e78be6118a
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-07 15:34:48 -0300

    :wrench: feat: Ajustes nav-bar

--- src/app/nav-bar/options-profile/options-profile.component.ts
+++ src/app/nav-bar/options-profile/options-profile.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {
+  faMagnifyingGlass,
+  faBell,
+  faCaretDown,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-options-profile',
   templateUrl: './options-profile.component.html',
-  styleUrl: './options-profile.component.css'
+  styleUrl: './options-profile.component.css',
 })
 export class OptionsProfileComponent {
-
+  magnifyingGlass = faMagnifyingGlass;
+  bell = faBell;
+  caretDown = faCaretDown;
 }
